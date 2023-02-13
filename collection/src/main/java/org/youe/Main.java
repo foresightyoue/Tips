25cdda0eebfdca3800bafcc62c54ff05fa5c8eee
foresightyoue <314837993@qq.com>
2023-02-13 09:34:24 +0000

    string.concat

--- collection/src/main/java/org/youe/Main.java
+++ collection/src/main/java/org/youe/Main.java
@@ -1,7 +1,17 @@
 package org.youe;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static final String TIME_FORMAT_HOURS_MINUTES = "HH:mm";
+
     public static void main(String[] args) {
+        String trueSql = " AND r.status = 1 AND r.online = 1 ";
+        System.out.println(trueSql.concat(""));
         System.out.println("Hello world!");
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT_HOURS_MINUTES);
+        System.out.println(dateTimeFormatter.format(time));
     }
 }
--- src/main/java/org/youe/Main.java
+++ src/main/java/org/youe/Main.java
@@ -2,6 +2,8 @@ package org.youe;
 
 public class Main {
     public static void main(String[] args) {
+        String trueSql = " AND r.status = 1 AND r.online = 1 ";
+        System.out.println(trueSql.concat(null));
         System.out.println("Hello world!");
     }
 }
